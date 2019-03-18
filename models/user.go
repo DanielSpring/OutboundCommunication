d903a0afe3a306f96e646b1546893444e7611a62
DanielSpring <daniel.spring@hotmail.co.uk>
2019-03-18 17:34:04 +0000

    test

--- models/user.go
+++ models/user.go
@@ -5,9 +5,11 @@ import "gopkg.in/mgo.v2/bson"
 type (
 	// User represents the structure of our resource
 	User struct {
-		Id     bson.ObjectId `json:"id" bson:"_id"`
-		Name   string        `json:"name" bson:"name"`
-		Gender string        `json:"gender" bson:"gender"`
-		Age    int           `json:"age" bson:"age"`
+		//Id     bson.ObjectId `json:"id" bson:"_id"`
+		firstName   string        `json:"firstName" bson:"firstName"`
+		lastName   string        `json:"lastName" bson:"lastName"`
+		message   string        `json:"message" bson:"message"`
+		email   string        `json:"email" bson:"email"`
+		service string        `json:"service" bson:"service"
 	}
 )
